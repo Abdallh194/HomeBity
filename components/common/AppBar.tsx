8630238adcd222c807f6d2a104702f758aab8989
Abdallh <Abdallhsabry194@gmail.com>
2024-10-17 21:43:53 +0300

    enhance

--- components/common/AppBar.tsx
+++ components/common/AppBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Container, Navbar, Nav, NavLink } from "react-bootstrap";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,25 +8,24 @@ function AppBar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="Navbar">
       <Container style={{ maxWidth: "1750px" }}>
-        <motion.div
+        {/* <motion.div
           initial={{ x: -30, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1 }}
+        ></motion.div> */}
+        <Link
+          href="/"
+          className="logo d-flex align-items-center txtDecoration_None"
         >
-          <Link
-            href="/"
-            className="logo d-flex align-items-center txtDecoration_None"
-          >
-            <Image
-              src="/logo.png"
-              alt="logo"
-              width={70}
-              height={30}
-              className="img-fluid"
-            />
-            Homebity
-          </Link>
-        </motion.div>
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={70}
+            height={30}
+            className="img-fluid"
+          />
+          Homebity
+        </Link>
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
